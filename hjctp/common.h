35812acba7523d46fe37df89331aadf01c36b340
lionhuo <huopan820@gmail.com>
2017-01-17 21:37:50 +0800

    fix the common.h

--- hjctp/common.h
+++ hjctp/common.h
@@ -13,18 +13,18 @@ using namespace std;
 class Common{
 public:
 	template <typename T> static void loadClass(JNIEnv *env, jclass *cls, jobject *obj, const char* filedName, int type, T value){
-		//if(type == 0){//string
-		//	jfieldID field = env->GetFieldID(*cls, filedName, "Ljava/lang/String;");
-		//	env->SetObjectField(*obj, field, value);
-		//}else if(type == 1){//double
-		//	jfieldID field = env->GetFieldID(*cls, filedName, "D");
-		//	env->SetDoubleField(*obj, field, atof(value));
-		//}else if(type == 2){//int
-		//	jfieldID field = env->GetFieldID(*cls, filedName, "I");
-		//	env->SetIntField(*obj, field, atoi(value));
-		//}else if(type == 3){//char
-		//	jfieldID field = env->GetFieldID(*cls, filedName, "C");
-		//	env->SetCharField(*obj, field, value.c_srt());		
-		//}
+		if(type == 0){//string
+			jfieldID field = env->GetFieldID(*cls, filedName, "Ljava/lang/String;");
+			env->SetObjectField(*obj, field, value);
+		}else if(type == 1){//double
+			jfieldID field = env->GetFieldID(*cls, filedName, "D");
+			env->SetDoubleField(*obj, field, atof(value));
+		}else if(type == 2){//int
+			jfieldID field = env->GetFieldID(*cls, filedName, "I");
+			env->SetIntField(*obj, field, atoi(value));
+		}else if(type == 3){//char
+			jfieldID field = env->GetFieldID(*cls, filedName, "C");
+			env->SetCharField(*obj, field, value.c_srt());		
+		}
 	}
 };
--- hjctp/mdSpi.cpp
+++ hjctp/mdSpi.cpp
@@ -33,96 +33,96 @@ void MdSpi::notifyFrontConnected(){
 void MdSpi::OnFrontDisconnected(int nReason){
 }
 
-//void MdSpi::reqUserLogin(){
-//	CThostFtdcReqUserLoginField req;
-//	memset(&req, 0, sizeof(req));
-//	strcpy(req.BrokerID, BROKER_ID);
-//	strcpy(req.UserID, INVESTOR_ID);
-//	strcpy(req.Password, PASSWORD);
-//	int iResult = mdApi->ReqUserLogin(&req, ++iMdRequestID);
-//	//cerr << "--->>> send login: " << ((iResult == 0) ? "success" : "fail") << " iResult = " << iResult << endl;
-//}
-//
-//void MdSpi::OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast){	
-//	notifyRspUserLogin(pRspUserLogin, pRspInfo, nRequestID, bIsLast);
-//	subScribeMarketData();
-//}
-//
-//void MdSpi::subScribeMarketData(){
-//	int iResult = mdApi->SubscribeMarketData(ppInstrumentID, nCount);
-//	//cerr << "--->>> send market data: " << ((iResult == 0) ? "success" : "failed") << endl;
-//}
-//
-//void MdSpi::OnRspSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast){
-//}
-//
-//void MdSpi::OnRtnDepthMarketData(CThostFtdcDepthMarketDataField *pDepthMarketData){
-//	notifyRtnDepthMarketData(pDepthMarketData);
-//}
-//
-//void MdSpi::notifyRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast){
-//	JNIEnv *env; 
-//	mdJvm->AttachCurrentThread((void **)&env, NULL);
-//	jclass mdSpiCls = env->GetObjectClass(jMdSpi);
-//	jmethodID methodid = env->GetMethodID(mdSpiCls, "onRspUserLogin", "(Lorg/zhps/hjctp/entity/CThostFtdcRspUserLoginField;Lorg/zhps/hjctp/entity/CThostFtdcRspInfoField;IZ)V");
-//
-//	if(pRspUserLogin == 0){
-//		env->CallVoidMethod(jMdSpi, methodid, NULL, NULL, nRequestID, bIsLast);
-//		mdJvm->DetachCurrentThread();
-//		return;
-//	}
-//
-//	jclass rspUserLoginCls = env->FindClass("org/zhps/hjctp/entity/CThostFtdcRspUserLoginField");
-//	jobject rspUserLoginObj = env->AllocObject(rspUserLoginCls); 
-//	//type: 0.string, 1.double, 2.int
-//	Common::loadClass(env, &rspUserLoginCls, &rspUserLoginObj, "tradingDay", 0, env->NewStringUTF(pRspUserLogin->TradingDay),0, 0);
-//	Common::loadClass(env, &rspUserLoginCls, &rspUserLoginObj, "loginTime", 0, env->NewStringUTF(pRspUserLogin->LoginTime),0, 0);
-//	Common::loadClass(env, &rspUserLoginCls, &rspUserLoginObj, "userId", 0, env->NewStringUTF(pRspUserLogin->UserID),0, 0);
-//	Common::loadClass(env, &rspUserLoginCls, &rspUserLoginObj, "shfeTime", 0, env->NewStringUTF(pRspUserLogin->SHFETime),0, 0);
-//
-//	jclass rspInfoCls = env->FindClass("org/zhps/hjctp/entity/CThostFtdcRspInfoField");
-//	jobject rspInfoObj = env->AllocObject(rspInfoCls);
-//
-//	env->CallVoidMethod(jMdSpi, methodid, rspUserLoginObj, rspInfoObj, nRequestID, bIsLast);
-//
-//	mdJvm->DetachCurrentThread();
-//}
-//
-//void MdSpi::notifyRtnDepthMarketData(CThostFtdcDepthMarketDataField *pDepthMarketData){
-//	JNIEnv *env; 
-//	mdJvm->AttachCurrentThread((void **)&env, NULL);
-//	jclass mdSpiCls = env->GetObjectClass(jMdSpi);
-//	jmethodID methodid = env->GetMethodID(mdSpiCls, "onRtnDepthMarketData", "(Lorg/zhps/hjctp/entity/CThostFtdcDepthMarketDataField;)V");
-//
-//	if(pDepthMarketData == 0){
-//		env->CallVoidMethod(jMdSpi, methodid, NULL);
-//		mdJvm->DetachCurrentThread();
-//		return;
-//	}
-//
-//	jclass rtnDepthMarketDataCls = env->FindClass("org/zhps/hjctp/entity/CThostFtdcDepthMarketDataField");
-//	jobject rtnDepthMarketDataObj =env->AllocObject(rtnDepthMarketDataCls); 
-//	//type: 0.string, 1.double, 2.int
-//	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "instrumentId", 0, env->NewStringUTF(pDepthMarketData->InstrumentID),0, 0);
-//	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "lastPrice", 1, NULL,pDepthMarketData->LastPrice, 0);
-//	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "preSettlementPrice", 1, NULL,pDepthMarketData->PreSettlementPrice, 0);
-//	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "preClosePrice", 1, NULL,pDepthMarketData->PreClosePrice, 0);
-//	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "preOpenInterest", 1, NULL,pDepthMarketData->PreOpenInterest, 0);
-//	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "openPrice", 1, NULL,pDepthMarketData->OpenPrice, 0);
-//	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "highestPrice", 1, NULL,pDepthMarketData->HighestPrice, 0);
-//	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "lowestPrice", 1, NULL,pDepthMarketData->LowestPrice, 0);
-//	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "volume", 2, NULL,0, pDepthMarketData->Volume);
-//	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "turnover", 1, NULL,pDepthMarketData->Turnover, 0);
-//	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "openInterest", 1, NULL,pDepthMarketData->OpenInterest, 0);
-//	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "closePrice", 1, NULL,pDepthMarketData->ClosePrice, 0);
-//	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "settlementPrice", 1, NULL,pDepthMarketData->SettlementPrice, 0);
-//	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "upperLimitPrice", 1, NULL,pDepthMarketData->UpperLimitPrice, 0);
-//	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "lowerLimitPrice", 1, NULL,pDepthMarketData->LowerLimitPrice, 0);
-//	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "updateTime", 0, env->NewStringUTF(pDepthMarketData->UpdateTime),0, 0);
-//	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "tradingDay", 0, env->NewStringUTF(pDepthMarketData->TradingDay),0, 0);
-//	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "updateMillisec", 2, NULL,0, pDepthMarketData->UpdateMillisec);
-//
-//	env->CallVoidMethod(jMdSpi, methodid, rtnDepthMarketDataObj);
-//
-//	mdJvm->DetachCurrentThread();
-//}
+void MdSpi::reqUserLogin(){
+	CThostFtdcReqUserLoginField req;
+	memset(&req, 0, sizeof(req));
+	strcpy(req.BrokerID, BROKER_ID);
+	strcpy(req.UserID, INVESTOR_ID);
+	strcpy(req.Password, PASSWORD);
+	int iResult = mdApi->ReqUserLogin(&req, ++iMdRequestID);
+	//cerr << "--->>> send login: " << ((iResult == 0) ? "success" : "fail") << " iResult = " << iResult << endl;
+}
+
+void MdSpi::OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast){	
+	notifyRspUserLogin(pRspUserLogin, pRspInfo, nRequestID, bIsLast);
+	subScribeMarketData();
+}
+
+void MdSpi::subScribeMarketData(){
+	int iResult = mdApi->SubscribeMarketData(ppInstrumentID, nCount);
+	//cerr << "--->>> send market data: " << ((iResult == 0) ? "success" : "failed") << endl;
+}
+
+void MdSpi::OnRspSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast){
+}
+
+void MdSpi::OnRtnDepthMarketData(CThostFtdcDepthMarketDataField *pDepthMarketData){
+	notifyRtnDepthMarketData(pDepthMarketData);
+}
+
+void MdSpi::notifyRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast){
+	JNIEnv *env; 
+	mdJvm->AttachCurrentThread((void **)&env, NULL);
+	jclass mdSpiCls = env->GetObjectClass(jMdSpi);
+	jmethodID methodid = env->GetMethodID(mdSpiCls, "onRspUserLogin", "(Lorg/zhps/hjctp/entity/CThostFtdcRspUserLoginField;Lorg/zhps/hjctp/entity/CThostFtdcRspInfoField;IZ)V");
+
+	if(pRspUserLogin == 0){
+		env->CallVoidMethod(jMdSpi, methodid, NULL, NULL, nRequestID, bIsLast);
+		mdJvm->DetachCurrentThread();
+		return;
+	}
+
+	jclass rspUserLoginCls = env->FindClass("org/zhps/hjctp/entity/CThostFtdcRspUserLoginField");
+	jobject rspUserLoginObj = env->AllocObject(rspUserLoginCls); 
+	//type: 0.string, 1.double, 2.int
+	Common::loadClass(env, &rspUserLoginCls, &rspUserLoginObj, "tradingDay", 0, pRspUserLogin->TradingDay);
+	Common::loadClass(env, &rspUserLoginCls, &rspUserLoginObj, "loginTime", 0, pRspUserLogin->LoginTime);
+	Common::loadClass(env, &rspUserLoginCls, &rspUserLoginObj, "userId", 0, pRspUserLogin->UserID);
+	Common::loadClass(env, &rspUserLoginCls, &rspUserLoginObj, "shfeTime", 0, pRspUserLogin->SHFETime);
+
+	jclass rspInfoCls = env->FindClass("org/zhps/hjctp/entity/CThostFtdcRspInfoField");
+	jobject rspInfoObj = env->AllocObject(rspInfoCls);
+
+	env->CallVoidMethod(jMdSpi, methodid, rspUserLoginObj, rspInfoObj, nRequestID, bIsLast);
+
+	mdJvm->DetachCurrentThread();
+}
+
+void MdSpi::notifyRtnDepthMarketData(CThostFtdcDepthMarketDataField *pDepthMarketData){
+	JNIEnv *env; 
+	mdJvm->AttachCurrentThread((void **)&env, NULL);
+	jclass mdSpiCls = env->GetObjectClass(jMdSpi);
+	jmethodID methodid = env->GetMethodID(mdSpiCls, "onRtnDepthMarketData", "(Lorg/zhps/hjctp/entity/CThostFtdcDepthMarketDataField;)V");
+
+	if(pDepthMarketData == 0){
+		env->CallVoidMethod(jMdSpi, methodid, NULL);
+		mdJvm->DetachCurrentThread();
+		return;
+	}
+
+	jclass rtnDepthMarketDataCls = env->FindClass("org/zhps/hjctp/entity/CThostFtdcDepthMarketDataField");
+	jobject rtnDepthMarketDataObj =env->AllocObject(rtnDepthMarketDataCls); 
+	//type: 0.string, 1.double, 2.int
+	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "instrumentId", 0, pDepthMarketData->InstrumentID);
+	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "lastPrice", 1, pDepthMarketData->LastPrice);
+	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "preSettlementPrice", 1, pDepthMarketData->PreSettlementPrice);
+	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "preClosePrice", 1, pDepthMarketData->PreClosePrice);
+	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "preOpenInterest", 1, pDepthMarketData->PreOpenInterest);
+	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "openPrice", 1, pDepthMarketData->OpenPrice);
+	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "highestPrice", 1, pDepthMarketData->HighestPrice);
+	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "lowestPrice", 1, pDepthMarketData->LowestPrice);
+	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "volume", 2, pDepthMarketData->Volume);
+	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "turnover", 1, pDepthMarketData->Turnover);
+	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "openInterest", 1, pDepthMarketData->OpenInterest);
+	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "closePrice", 1, pDepthMarketData->ClosePrice);
+	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "settlementPrice", 1, pDepthMarketData->SettlementPrice);
+	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "upperLimitPrice", 1, pDepthMarketData->UpperLimitPrice);
+	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "lowerLimitPrice", 1, pDepthMarketData->LowerLimitPrice);
+	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "updateTime", 0, pDepthMarketData->UpdateTime);
+	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "tradingDay", 0, pDepthMarketData->TradingDay);
+	Common::loadClass(env, &rtnDepthMarketDataCls, &rtnDepthMarketDataObj, "updateMillisec", 2, pDepthMarketData->UpdateMillisec);
+
+	env->CallVoidMethod(jMdSpi, methodid, rtnDepthMarketDataObj);
+
+	mdJvm->DetachCurrentThread();
+}
