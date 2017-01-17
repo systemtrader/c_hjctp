2a0f06b44cadad5a8baa7d26c4910ee28395961c
lionhuo <huopan820@gmail.com>
2017-01-17 22:44:32 +0800

    update

--- hjctp/common.h
+++ hjctp/common.h
@@ -13,6 +13,7 @@ using namespace std;
 class Common{
 public:
 	template <typename T> static void loadClass(JNIEnv *env, jclass *cls, jobject *obj, const char* filedName, int type, T val){
+		//cerr << "value: " << val << endl;
 		if(type == 0){//string
 			jfieldID field = env->GetFieldID(*cls, filedName, "Ljava/lang/String;");
 			env->SetObjectField(*obj, field, (jobject)val);
